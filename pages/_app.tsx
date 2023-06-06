7346f10856f38b2e26f4eda7daea1e1f7c22719e
Qing <cwq1913@gmail.com>
2023-06-06 22:48:18 +0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
-import { Analytics } from "@vercel/analytics/react";
-import "./style.css";
+import "./style.css"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
